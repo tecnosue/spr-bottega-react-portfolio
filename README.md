5b4ea9cefba2f4b26359344d143c22148a604507
Susana Paracuellos <82464797+tecnosue@users.noreply.github.com>
2022-07-20 23:20:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Susana Paracuellos React Portfolio Application
---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import moment from "moment";
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Susana Paracuellos</h1>
+        <h2>Portfolio</h2>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+      </div>
+    );
+  }
+}
